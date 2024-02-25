2ed521fedc4fee90d69194cddb7c6b605c56e4f6
dasdeo <mark@anio.software>
2024-02-25 11:58:56 +0100

    Remove "checkAPICompatibility" step since @anio-jtest/test will be included in this package.

--- src/steps/index.mjs
+++ src/steps/index.mjs
@@ -1,18 +1,16 @@
 import checkInput from "./0.checkInput.mjs"
-import checkAPICompatibility from "./1.checkAPICompatibility.mjs"
-import importAllTestFiles from "./2.importAllTestFiles.mjs"
-import computeUnits from "./3.computeUnits.mjs"
-import computeNumberOfTestsToRun from "./4.computeNumberOfTestsToRun.mjs"
-import spawnRunners from "./5.spawnRunners.mjs"
-import waitForAllRunnersToBeReady from "./6.waitForAllRunnersToBeReady.mjs"
-import mapUnitsToRunners from "./7.mapUnitsToRunners.mjs"
-import killRunners from "./8.killRunners.mjs"
-import sanityCheck from "./9.sanityCheck.mjs"
-import statistics from "./10.statistics.mjs"
+import importAllTestFiles from "./1.importAllTestFiles.mjs"
+import computeUnits from "./2.computeUnits.mjs"
+import computeNumberOfTestsToRun from "./3.computeNumberOfTestsToRun.mjs"
+import spawnRunners from "./4.spawnRunners.mjs"
+import waitForAllRunnersToBeReady from "./5.waitForAllRunnersToBeReady.mjs"
+import mapUnitsToRunners from "./6.mapUnitsToRunners.mjs"
+import killRunners from "./7.killRunners.mjs"
+import sanityCheck from "./8.sanityCheck.mjs"
+import statistics from "./9.statistics.mjs"
 
 export default [
 	checkInput,
-	checkAPICompatibility,
 	importAllTestFiles,
 	computeUnits,
 	computeNumberOfTestsToRun,
